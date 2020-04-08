2ca274401b0070c4467f575775e87c0eaca1e6f3
Radoto <radotooo@gmail.com>
2020-04-09 00:44:15 +0300

    Initial commit

---
+++ WodApp/WodApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WodApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
